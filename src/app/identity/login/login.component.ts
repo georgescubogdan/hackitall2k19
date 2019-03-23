69421f67fe11fc35f90d5a9bc0059e9b41e1d647
ParaschivGeorge <georgegmtt@gmail.com>
2019-03-23 13:51:18 +0200

    login component

--- src/app/identity/login/login.component.ts
+++ src/app/identity/login/login.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroupDirective, NgForm, Validators, FormGroup} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+import { UserService } from '../services/user.service';
+import { Router } from '@angular/router';
 
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.sass']
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
 
-  constructor() { }
+  passwordFormControl = new FormControl('', [
+    Validators.required
+  ]);
+
+  matcher = new MyErrorStateMatcher();
+
+  constructor(private _userService: UserService, private _router: Router) { }
 
   ngOnInit() {
+    this.loginForm = new FormGroup({'email': this.emailFormControl, 'password': this.passwordFormControl});
   }
 
+  onSubmit() {
+    if (this.loginForm.valid) {
+    }
+  }
 }
