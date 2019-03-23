24bc0d91040e263d8255c2791117025b7f317f1e
ParaschivGeorge <georgegmtt@gmail.com>
2019-03-23 15:19:20 +0200

    remove userservice

--- src/app/identity/register/register.component.ts
+++ src/app/identity/register/register.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroupDirective, NgForm, Validators, FormGroup} from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
-import { UserService } from '../services/user.service';
 import { Router } from '@angular/router';
 
 /** Error when invalid control is dirty, touched, or submitted. */
